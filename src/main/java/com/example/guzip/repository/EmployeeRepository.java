2a78cd52bf33cdd7b7c2fdbd622021c69a925c82
cie10 <leesky0075@dgu.ac.kr>
2025-02-03 21:53:22 +0900

    feat: implement Entity and Repository (#6)

--- src/main/java/com/example/guzip/repository/EmployeeRepository.java
+++ src/main/java/com/example/guzip/repository/EmployeeRepository.java
@@ -1,8 +1,26 @@
 package com.example.guzip.repository;
 
 import com.example.guzip.entity.user.Employee;
+import com.example.guzip.entity.user.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface EmployeeRepository extends JpaRepository<Employee, Long> {
-    //기본적으로 제공하는 findById 메서드 사용
+
+    /**
+     * 특정 User가 Employee(직원)인지 확인
+     *
+     * @param user 사용자 객체
+     * @return Employee 존재 여부
+     */
+    boolean existsByUser(User user);
+
+    /**
+     * 특정 User와 연결된 Employee 정보를 가져오는 메서드
+     *
+     * @param user 사용자 객체
+     * @return Employee 정보(Optional)
+     */
+    Optional<Employee> findByUser(User user);
 }
